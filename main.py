81948acdd62a98c4989f22d6afe178805163cba1
AnuragNegii <Negianurag99@gmail.com>
2026-07-15 08:16:09 +0530

    just basics of pygame start

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello from alien-invasion!")
+import sys
+import pygame
+
+class AlienInvasion:
+    def __init__(self) -> None:
+        pygame.init()
+
+        self.clock = pygame.time.Clock()
+        
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien invasion")
+
+        self.bg_color = (230, 230, 230)
+
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            self.screen.fill(self.bg_color)
+            pygame.display.flip()
+            self.clock.tick(60)
 
 
 if __name__ == "__main__":
-    main()
+    ai = AlienInvasion()
+    ai.run_game()
